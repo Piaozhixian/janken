b13205357ad232ec006867cc400ce26cdf11a001
Piao <pakushiken@gmail.com>
2022-06-21 23:29:53 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Zhixian Piao on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
